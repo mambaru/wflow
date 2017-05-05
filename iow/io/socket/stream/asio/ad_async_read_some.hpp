8f4e2b0b70c045dc64c282a70f5108abd3dd67a0
Vladimir Migashko <migashko@gmail.com>
2017-05-05 13:11:27 +0300

    upd

--- iow/io/socket/stream/asio/ad_async_read_some.hpp
+++ iow/io/socket/stream/asio/ad_async_read_some.hpp
@@ -1,19 +1,21 @@
 #pragma once
 
 #include <iow/asio.hpp>
+#include <iow/system.hpp>
 #include <utility>
 
 namespace iow{ namespace io{ namespace socket{ namespace stream{ namespace asio{
 
 struct ad_async_read_some1
 {
+  typedef std::function<void(::iow::system::error_code, std::size_t)> user_handler;
   template<typename T, typename P, typename H>
   void operator()(T& t, P p, H&& handler)
   {
     using namespace std::placeholders;
     t.descriptor().async_read_some(
       ::iow::asio::buffer( p.first, p.second ),
-      std::bind(handler, _1, _2, nullptr)
+      std::bind(handler, _1, _2, user_handler())
       //std::forward<H>(handler)
     );
   }
