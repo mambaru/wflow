292c79b6b0736ba1e7a8d29d3f6a70c550e8cad0
Vladimir Migashko <migashko@gmail.com>
2017-05-11 20:01:48 +0300

    +recommended_for method

--- iow/io/reader/data/ad_confirm.hpp
+++ iow/io/reader/data/ad_confirm.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iow/io/reader/data/tags.hpp>
+#include <iow/logger/logger.hpp>
 #include <memory>
 
 namespace iow{ namespace io{ namespace reader{ namespace data{
@@ -13,7 +14,7 @@ struct ad_confirm
     bool result = t.get_aspect().template get<_read_buffer_>().confirm( std::move(p) );
     if ( !result )
     {
-      
+      IOW_LOG_ERROR("iow::io::reader::data::ad_confirm: ошибка буфера")
       // read error
     }
   }
