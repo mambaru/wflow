6d3c5d8052d74f024d566e544453ea756f65a4af
Vladimir Migashko <migashko@gmail.com>
2017-08-15 00:51:35 +0300

    странная ошибка iow/io/writer/data/options.hpp size_t maxsize = 0;

--- iow/io/writer/data/options.hpp
+++ iow/io/writer/data/options.hpp
@@ -8,6 +8,10 @@ namespace iow{ namespace io{ namespace writer{ namespace data{
 struct options:
   ::iow::io::write_buffer_options
 {
+  //!!! Вынести на более высокий уровень
+#error **
+  size_t maxsize = 0;
+
   size_t wrnsize = 1024*1024;
 };
 
