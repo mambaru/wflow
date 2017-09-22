0ca6c5745f97e22c8ea4a288ea892cfaddd19b04
Vladimir Migashko <migashko@gmail.com>
2017-09-22 18:44:03 +0300

    debug

--- iow/ip/udp/server/server.hpp
+++ iow/ip/udp/server/server.hpp
@@ -7,30 +7,6 @@
 
 namespace iow{ namespace ip{ namespace udp{ namespace server{
 
-  /*
-template <typename ConnectionType, typename A = fas::empty_type>
-class acceptor
-  : public ::iow::io::descriptor::holder< typename fas::merge_aspect<A, aspect<ConnectionType> >::type >
-{
-  typedef ::iow::io::descriptor::holder< typename fas::merge_aspect<A, aspect<ConnectionType> >::type > super;
-public:
-  typedef ConnectionType connection_type;
-  typedef typename super::descriptor_type descriptor_type;
-  typedef typename super::mutex_type mutex_type;
-
-  acceptor(descriptor_type&& desc)
-    : super( std::forward<descriptor_type>(desc))
-  {}
-
-  template<typename O>
-  void listen(O&& opt)
-  {
-    std::lock_guard<mutex_type> lk( super::mutex() );
-    this->get_aspect().template get<_listen_>()(*this, std::forward<O>(opt) );
-  }
-};
-*/
-
 template <typename A = fas::empty_type>
 class worker_base
   : public ::iow::io::descriptor::holder< typename fas::merge_aspect<A, aspect >::type >
@@ -64,21 +40,17 @@ public:
 };
 
 template<typename Worker = worker >
-using server = ::iow::io::server::server<Worker>;
+using server_base = ::iow::io::server::server<Worker>;
 
-/*
-template<typename Worker = worker<> >
-using server = ::iow::io::server::server<Worker>;
-*/
+class server: public server_base<>
+{
+  typedef server_base<> super;
+public:
+  typedef super::io_service_type io_service_type; 
+  explicit server(io_service_type& io)
+    : super( io )
+  {}
+};
 
-/*
-template<typename A = fas::aspect<> >
-using server_base =
-  ::iow::io::socket::dgram::socket_base< typename fas::merge_aspect<A, aspect >::type >;
-
-template<typename A = fas::aspect<> >
-using server =
-  ::iow::io::socket::dgram::socket< typename fas::merge_aspect<A, aspect >::type >;
-*/
 
 }}}}
