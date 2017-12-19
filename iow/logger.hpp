b6c735fec4741e87bc093479b20ea7cac7e422a5
Vladimir Migashko <migashko@gmail.com>
2017-12-19 19:50:03 +0300

    logger->wlog

--- iow/logger.hpp
+++ iow/logger.hpp
@@ -8,19 +8,7 @@
 
 #ifndef IOW_DISABLE_LOG
 
-#include <wlog/logger.hpp>
-
-/*
-#define JSONRPC_LOG_ERROR(X)    IOW_WRITE_LOG_ERROR( "jsonrpc", X )
-#define JSONRPC_LOG_WARNING(X)  IOW_WRITE_LOG_WARNING( "jsonrpc", X )
-#define JSONRPC_LOG_MESSAGE(X)  IOW_WRITE_LOG_MESSAGE( "jsonrpc", X )
-#define JSONRPC_LOG_FATAL(X)    IOW_WRITE_LOG_FATAL( "jsonrpc", X )
-#define JSONRPC_LOG_BEGIN(X)    IOW_WRITE_LOG_BEGIN( "jsonrpc", X )
-#define JSONRPC_LOG_END(X)      IOW_WRITE_LOG_END( "jsonrpc", X )
-#define JSONRPC_LOG_DEBUG(X)    IOW_WRITE_LOG_DEBUG( "jsonrpc", X )
-#define JSONRPC_LOG_TRACE(X)    IOW_WRITE_LOG_TRACE( "jsonrpc", X )
-#define JSONRPC_LOG_PROGRESS(X) IOW_WRITE_LOG_PROGRESS( "jsonrpc", X )
-*/
+#include <wlog/logging.hpp>
 
 #define IOW_LOG_ERROR(X)    WLOG_LOG_ERROR( "IOW", X )
 #define IOW_LOG_WARNING(X)  WLOG_LOG_WARNING( "IOW", X )
