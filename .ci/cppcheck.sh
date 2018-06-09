d22da477ee06701ebdd943bd853c331acbc2e879
Vladimir Migashko <migashko@gmail.com>
2018-06-09 18:38:51 +0300

    *cppcheck-ci

--- .ci/cppcheck.sh
+++ .ci/cppcheck.sh
@@ -4,6 +4,6 @@ script_dir=$(dirname $script_name)
 prj_dir=$(dirname $script_dir)
 
 echo "$prj_dir"
-$script_dir/cppcheck-exec.sh $@ $prj_dir |& tee /tmp/cppcheck.cppcheck 
-sh -c "! grep '\[' /tmp/cppcheck.cppcheck"
+$script_dir/cppcheck-exec.sh $@ $prj_dir |& tee ./cppcheck.cppcheck 
+sh -c "! grep '\[' ./cppcheck.cppcheck"
 exit $?
