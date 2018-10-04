6c5df3303da6ab6d0cc88e2a5562555f47f07aeb
Vladimir Migashko <migashko@gmail.com>
2018-10-04 15:47:58 +0300

    check unsafe size only

--- wflow/workflow.cpp
+++ wflow/workflow.cpp
@@ -222,20 +222,21 @@ void workflow::create_wrn_timer_(const workflow_options& opt)
       control_handler= [this, wrnsize, dropsave, debug]()  ->bool 
       {
         auto dropcount = this->_impl->dropped();
-        auto size = this->_impl->full_size();
+        auto size = this->_impl->unsafe_size();
+        auto safe_size = this->_impl->safe_size();
         auto dropdiff = dropcount - *dropsave;
         if ( dropdiff!=0 )
         {
-          WFLOW_LOG_ERROR("Workflow '" << this->_id << "' queue dropped " << dropdiff << " items (total " << dropcount << ", size " << size << ")" )
+          WFLOW_LOG_ERROR("Workflow '" << this->_id << "' queue dropped " << dropdiff << " items (total " << dropcount << ", size " << size << ", safe_size " << safe_size <<  ")" )
           *dropsave = dropcount;
         }
         else if ( size > wrnsize )
         {
-          WFLOW_LOG_WARNING("Workflow '" << this->_id << "' queue size warning. Size " << size << " (wrnsize=" << wrnsize << ")")
+          WFLOW_LOG_WARNING("Workflow '" << this->_id << "' queue size warning. Size " << size << " safe_size " << safe_size << " (wrnsize=" << wrnsize << ")")
         } 
         else if ( debug )
         {
-          WFLOW_LOG_MESSAGE("Workflow '" << this->_id << "' debug: total dropped " << dropcount << ", queue size " << size)
+          WFLOW_LOG_MESSAGE("Workflow '" << this->_id << "' debug: total dropped " << dropcount << ", queue size=" << size << " + safe_size=" << safe_size )
         }
         return true;
       };
